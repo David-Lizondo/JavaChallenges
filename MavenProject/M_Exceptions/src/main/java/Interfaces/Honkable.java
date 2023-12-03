34367e7da51898548acb52823f6be8e44e2ec896
David Lizondo <davidlizondobap@gmail.com>
2023-12-03 18:56:42 -0300

    M_Exceptions has been updated

---
+++ MavenProject/M_Exceptions/src/main/java/Interfaces/Honkable.java
@@ -0,0 +1,13 @@
+
+package Interfaces;
+
+public interface Honkable {
+
+    void honkHorn();
+    
+    void playMelody(String melody);
+    
+    boolean isHornWorking();
+}
+    
+
---
+++ MavenProject/M_Exceptions/src/main/java/model_exceptions/HornMalfunctionException.java
@@ -0,0 +1,7 @@
+package model_exceptions;
+
+public class HornMalfunctionException extends Exception {
+    public HornMalfunctionException(String message) {
+        super(message);
+    }
+}
