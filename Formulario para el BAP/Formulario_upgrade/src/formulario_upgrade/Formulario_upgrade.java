77b4664a255aaf1c3183e9dd5f007cdeaa02838b
David Lizondo <davidlizondobap@gmail.com>
2024-03-03 19:38:31 -0300

    se borraron archivos

--- Formulario para el BAP/Formulario_upgrade/src/formulario_upgrade/Formulario_upgrade.java
+++
@@ -1,9 +0,0 @@
-package formulario_upgrade;
-
-public class Formulario_upgrade {
-
-    public static void main(String[] args) {
-        FormularioBAP formulario = new FormularioBAP();
-        formulario.iniciarFormulario();
-    }
-}
